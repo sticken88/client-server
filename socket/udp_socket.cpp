c7efc0c0dffc365b2b8115bbb3c0c1240537e0bc
sticken88 <matteo.sticco@gmail.com>
2015-01-24 13:07:53 -0800

    Created socket folder to store socket source files

---
+++ socket/udp_socket.cpp
@@ -0,0 +1,49 @@
+#include "udp_socket.h"
+
+ssize_t udp_socket::recv_data(void *bufptr, size_t nbytes, int flags, struct sockaddr *sa, socklen_t *salenptr)
+{
+   ssize_t n;
+
+   n = recvfrom(fd, bufptr, nbytes, flags, sa, salenptr);
+
+   if(n < 0){
+      printf("Error while receiving data, closing..\n");
+      exit(1);
+   }
+
+   return n;
+}
+
+void udp_socket::send_data(void *bufptr, size_t nbytes, int flags, const struct sockaddr *sa, socklen_t salen)
+{
+   size_t bytes_sent;
+
+   bytes_sent = sendto(fd, bufptr, nbytes, flags, sa, salen);
+
+   if(bytes_sent != nbytes){
+      printf("Error while sending data, closing\n");
+      exit(1);
+   }
+}
+
+int udp_socket::get_fd(void)
+{
+   return fd;
+}
+
+udp_socket::udp_socket(int family, int type, int protocol)
+{
+   fd = socket(family, type, protocol);
+   shut_mode = 2;
+
+   if(fd < 0){
+      printf("Error while creating socket, closing..\n");
+      exit(1);
+   }
+}
+
+udp_socket::~udp_socket()
+{
+   shutdown(fd, shut_mode);
+   close(fd);
+}
---
+++ socket/udp_socket.h
@@ -0,0 +1,29 @@
+#ifndef __UDP_SOCKET_H_
+#define __UDP_SOCKET_H_
+
+#include <stdio.h>
+#include <stdlib.h> // getenv()
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> // inet_aton()
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+
+class udp_socket
+{
+   public:
+      // send and receive functions
+      ssize_t recv_data(void *bufptr, size_t nbytes, int flags, struct sockaddr *sa, socklen_t *salenptr);
+      void send_data(void *bufptr, size_t nbytes, int flags, const struct sockaddr *sa, socklen_t salen);
+      int get_fd(void);
+      // constructor and distructor
+      udp_socket(int family, int type, int protocol);
+      ~udp_socket();
+   private:
+      int fd;
+      int shut_mode; // used to specify how to shut down the socket: 2 means both ways
+};
+
+#endif
