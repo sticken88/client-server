0218cacd65a8966d19241f202676fb01cfe7b9c5
sticken88 <matteo.sticco@gmail.com>
2015-01-24 13:48:57 -0800

    Added file header for fd_manager

---
+++ fd/fd_manager.h
@@ -0,0 +1,25 @@
+#ifndef __UDP_SOCKET_H_
+#define __UDP_SOCKET_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+
+#define READ_MODE O_RDONLY
+#define WRITE_MODE O_WRONLY | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR
+
+class fd_manager
+{
+   public:
+          void open_read(int *fd, char *file_name);
+          void open_write(int *fd, char *file_name);
+          void close_file(int *fd);
+          void remove_file(char *file_name);
+          // constructor
+          fd_manager(void);
+};
+
+#endif
